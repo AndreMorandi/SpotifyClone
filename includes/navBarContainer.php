2046015054633a4f662dcd0fe27b2d7fa6473dc0
AndreMorandi <40004531+AndreMorandi@users.noreply.github.com>
2018-08-28 13:31:29 -0300

    Update navBarContainer.php

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -2,29 +2,29 @@
 <div id="navBarContainer">
 	<nav class="navBar">
 
-		<a href="indexTest.php" class="logo"> <!--TO DO: rename the index page-->
+		<span role="link"  tabindex="0" onclick="openPage('index.php')" class="logo"> 
 			<img src="assets/images/icons/spotify.png" title="Spotify">					
-		</a>
+		</span>
 
 		<div class="group">
 			<div class="navItem">
-				<a href="search.php" class="navItemLink">Search
-					<img src="assets/images/icons/search.png" class="icon" alt="Search">
-				</a><!--Page Not Created Yet-->
+				<span role='link' tabindex='0' onclick='openPage("search.php")' class="navItemLink">Search
+					<span class="icon fa fa-search" ></span>
+				</span><!--Page Not Created Yet-->
 			</div>
 		</div>
 
 		<div class="group">
 			<div class="navItem">
-				<a href="#" class="navItemLink">Browse</a>
+				<span roles="link" tabindex="0" onclick="openPage('browse.php')" class="navItemLink">Browse</span>
 			</div>
 
 			<div class="navItem">
-				<a href="#" class="navItemLink">Your Music</a>
+				<span roles="link" tabindex="0" onclick="openPage('yourMusic.php')" class="navItemLink">Your Music</span>
 			</div>
 
 			<div class="navItem">
-				<a href="#" class="navItemLink">Profile</a>
+				<span roles="link" tabindex="0" onclick="openPage('profile.php')" class="navItemLink"><?php echo $userLoggedIn; ?></span>
 			</div>
 		</div>
 
